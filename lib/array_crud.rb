197f36fda572e73dfbe141a77a3a6c5bfe8ae9f3
Justine Berretta <justberretta@gmail.com>
2019-05-20 02:06:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [Ravenclaw, Slytherin, Hufflepuff, Gryffindor]
+  ["Ravenclaw", "Slytherin", "Hufflepuff", "Gryffindor"]
 end
 
 def add_element_to_end_of_array(array, element)
